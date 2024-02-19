a1395f83f80a2b66a6ac2dca364ee87318c4256e
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-02-19 15:44:18 +0100

    Calling a server action from a client component - editing a record

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from '@prisma/client';
 import Editor from '@monaco-editor/react';
 import { useState } from 'react';
+import * as actions from '@/actions';
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -15,6 +16,9 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
     setCode(value);
   };
 
+  // Make sure 'editSnippet' gets called with the id of the snippet and the code from editor
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
+
   return (
     <div>
       <Editor
@@ -25,6 +29,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
